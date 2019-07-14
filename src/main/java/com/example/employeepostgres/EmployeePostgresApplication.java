194a58b335c83b51262aaaf15515a60113f92c90
csuttic <chan.suttichujit@exxonmobil.com>
2019-07-14 13:05:31 +0700

    initial commit

---
+++ src/main/java/com/example/employeepostgres/EmployeePostgresApplication.java
@@ -0,0 +1,13 @@
+package com.example.employeepostgres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeePostgresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeePostgresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/employeepostgres/EmployeePostgresApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.employeepostgres;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EmployeePostgresApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
