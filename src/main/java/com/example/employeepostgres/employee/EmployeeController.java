3f637f97a7b165dd189ed3b785736ab82cdf740b
csuttic <chan.suttichujit@exxonmobil.com>
2019-07-14 14:29:15 +0700

    create get employee  by id controller

---
+++ src/main/java/com/example/employeepostgres/employee/EmployeeController.java
@@ -0,0 +1,21 @@
+package com.example.employeepostgres.employee;
+
+import com.example.employeepostgres.employee.dto.EmployeeDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeController {
+
+  @Autowired
+  private EmployeeService employeeService;
+
+  @GetMapping
+  public EmployeeDTO getEmployeeById(int employeeId){
+    return employeeService.getEmployeeById(employeeId);
+  }
+
+}
---
+++ src/main/java/com/example/employeepostgres/employee/EmployeeService.java
@@ -0,0 +1,12 @@
+package com.example.employeepostgres.employee;
+
+import com.example.employeepostgres.employee.dto.EmployeeDTO;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeService {
+
+  public EmployeeDTO getEmployeeById(int employeeId) {
+    return null;
+  }
+}
