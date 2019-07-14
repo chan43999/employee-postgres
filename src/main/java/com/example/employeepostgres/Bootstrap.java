445f09bc00befbb0fed1c2fbd7579965b169d566
csuttic <chan.suttichujit@exxonmobil.com>
2019-07-14 15:29:26 +0700

    configure expose employee endpoint

---
+++ src/main/java/com/example/employeepostgres/Bootstrap.java
@@ -0,0 +1,24 @@
+package com.example.employeepostgres;
+
+import com.example.employeepostgres.employee.EmployeeRepository;
+import com.example.employeepostgres.employee.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Bootstrap implements CommandLineRunner {
+
+  @Autowired
+  private EmployeeRepository employeeRepository;
+
+  @Override
+  public void run(String... args) throws Exception {
+
+    employeeRepository.save(Employee.builder()
+        .id(1l)
+        .firstName("Chan")
+        .lastName("Suttichujit")
+        .build());
+  }
+}
--- src/main/java/com/example/employeepostgres/employee/EmployeeController.java
+++ src/main/java/com/example/employeepostgres/employee/EmployeeController.java
@@ -3,18 +3,19 @@ package com.example.employeepostgres.employee;
 import com.example.employeepostgres.employee.dto.EmployeeDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/employee")
+@RequestMapping("/employees")
 public class EmployeeController {
 
   @Autowired
   private EmployeeService employeeService;
 
-  @GetMapping
-  public EmployeeDTO getEmployeeById(Long employeeId){
+  @GetMapping("/{employeeId}")
+  public EmployeeDTO getEmployeeById(@PathVariable Long employeeId){
     return employeeService.getEmployeeById(employeeId);
   }
 
